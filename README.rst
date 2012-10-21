97521a201dffb0538b1051a77c757fc3fe1de920
Ralf Schmitt <ralf@systemexit.de>
2012-10-22 00:21:14 +0200

    shorten command line options, ini options a bit, add README

---
+++ README.rst
@@ -0,0 +1,34 @@
+.. -*- mode: rst; coding: utf-8 -*-
+
+==============================================================================
+pytest-twisted - test twisted code with pytest
+==============================================================================
+
+
+:Authors: Ralf Schmitt <ralf@systemexit.de>
+:Version: 1.0
+:Date:    2012-10-21
+
+
+pytest-twisted is a plugin for pytest, which allows to test code,
+which uses the twisted framework. test functions can return Deferred
+objects and pytest will wait for their completion with this plugin.
+
+Using the plugin
+==================
+Install the plugin with::
+
+    pip install pytest-twisted
+
+The plugin must be enabled. This can be done in the following ways:
+
+1. Run py.test with the --twisted command line option
+
+2. Put the following into conftest.py::
+
+    pytest_plugins = "pytest_twisted"
+
+3. Put the following into pytest.ini::
+
+    [pytest]
+    twisted = 1
--- pytest_twisted/__init__.py
+++ pytest_twisted/__init__.py
@@ -2,14 +2,14 @@ class _loader_plugin(object):
 
     def pytest_addoption(self, parser):
         group = parser.getgroup("twisted", "run tests for a twisted application")
-        group.addoption("--with-twisted", action="store_true",
+        group.addoption("--twisted", action="store_true",
                         help="run tests with the twisted reactor")
-        group.addoption('--reactor', help="reactor to use", default=None)
-        parser.addini("with_twisted", "run tests with the twisted reactor")
+        # group.addoption('--reactor', help="reactor to use", default=None)
+        parser.addini("twisted", "run tests with the twisted reactor")
         parser.addini("reactor", "reactor to use")
 
     def pytest_configure(self, config):
-        enable = config.getvalue("with_twisted") or config.getini("with_twisted") in ("yes", "true", "on", "1")
+        enable = config.getvalue("twisted") or config.getini("twisted") in ("yes", "true", "on", "1")
         if enable:
             pytest_configure(config)
 
