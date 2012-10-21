7cebefe8913751d5a1af6925cbf84f8f013d4be0
Ralf Schmitt <ralf@systemexit.de>
2012-10-22 00:37:06 +0200

    cleanup whitespace, remove unused ini option

--- pytest_twisted/__init__.py
+++ pytest_twisted/__init__.py
@@ -1,3 +1,8 @@
+def pytest_configure(config):
+    from pytest_twisted import plugin
+    config.pluginmanager.register(plugin)
+
+
 class _loader_plugin(object):
 
     def pytest_addoption(self, parser):
@@ -6,7 +11,7 @@ class _loader_plugin(object):
                         help="run tests with the twisted reactor")
         # group.addoption('--reactor', help="reactor to use", default=None)
         parser.addini("twisted", "run tests with the twisted reactor")
-        parser.addini("reactor", "reactor to use")
+        # parser.addini("reactor", "reactor to use")
 
     def pytest_configure(self, config):
         enable = config.getvalue("twisted") or config.getini("twisted") in ("yes", "true", "on", "1")
@@ -14,7 +19,3 @@ class _loader_plugin(object):
             pytest_configure(config)
 
 _loader = _loader_plugin()
-
-def pytest_configure(config):
-    from pytest_twisted import plugin
-    config.pluginmanager.register(plugin)
--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@
 from setuptools import setup
 
 
-setup(
-    name='pytest-twisted',
-    version='1.0',
-    description='A twisted plugin for py.test.',
-    long_description=open("README.rst").read(),
-    author='Ralf Schmitt',
-    author_email='ralf@brainbot.com',
-    url='https://github.com/schmir/pytest-twisted',
-    packages=['pytest_twisted'],
-    install_requires=["greenlet", "pytest"],
-    classifiers=['Development Status :: 5 - Production/Stable',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Topic :: Software Development :: Testing'],
-    entry_points=dict(pytest11=['twisted = pytest_twisted:_loader']))
+setup(name='pytest-twisted',
+      version='1.0',
+      description='A twisted plugin for py.test.',
+      long_description=open("README.rst").read(),
+      author='Ralf Schmitt',
+      author_email='ralf@brainbot.com',
+      url='https://github.com/schmir/pytest-twisted',
+      packages=['pytest_twisted'],
+      install_requires=["greenlet", "pytest"],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Testing'],
+      entry_points=dict(pytest11=['twisted = pytest_twisted:_loader']))
