b21ff12c09100e57d4d96504182a14e350b9970f
Ralf Schmitt <ralf@systemexit.de>
2012-11-26 00:40:42 +0100

    up version to 1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='pytest-twisted',
-      version='1.3',
+      version='1.4',
       description='A twisted plugin for py.test.',
       long_description=open("README.rst").read(),
       author='Ralf Schmitt',
