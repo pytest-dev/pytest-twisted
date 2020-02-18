277a4e9d47f44849f13fe99a53a44a34360aaf1f
Kyle Altendorf <sda@fstab.net>
2020-02-18 15:44:06 -0500

    Use asyncio.WindowsSelectorEventLoopPolicy() for applicable tests
    https://twistedmatrix.com/trac/ticket/9766

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,1 +1,20 @@
+import sys
+
+import pytest
+
+
 pytest_plugins = "_pytest.pytester"
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    if (
+            config.getoption("reactor") == 'asyncio'
+            and sys.platform == 'win32'
+            and sys.version_info >= (3, 8)
+    ):
+        # https://twistedmatrix.com/trac/ticket/9766
+        import asyncio
+
+        selector_policy = asyncio.WindowsSelectorEventLoopPolicy()
+        asyncio.set_event_loop_policy(selector_policy)
