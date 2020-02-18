c6eff3da4433f7e220190475bbe7a91af2f96fda
Kyle Altendorf <sda@fstab.net>
2020-02-17 20:42:00 -0500

    Use asyncio.WindowsSelectorEventLoopPolicy for 3.8+
    https://twistedmatrix.com/trac/ticket/9766

--- pytest_twisted.py
+++ pytest_twisted.py
@@ -272,6 +272,12 @@ def init_qt5_reactor():
 
 
 def init_asyncio_reactor():
+    if sys.version_info >= (3, 8):
+        # If twisted releases a fix/workaround we can check that version too
+        # https://twistedmatrix.com/trac/ticket/9766
+        selector_policy = asyncio.WindowsSelectorEventLoopPolicy()
+        asyncio.set_event_loop_policy(selector_policy)
+    
     from twisted.internet import asyncioreactor
 
     _install_reactor(
