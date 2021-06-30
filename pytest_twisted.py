37f0baf26eca91197deb0aec4a09d76f6179c9e7
Jan Matějka <jan@matejka.ninja>
2021-06-30 15:45:06 +0200

    fix process hanging due to reactor.stop() being called twice on sigint

--- pytest_twisted.py
+++ pytest_twisted.py
@@ -208,7 +208,15 @@ def init_twisted_greenlet():
 
 def stop_twisted_greenlet():
     if _instances.gr_twisted:
-        _instances.reactor.stop()
+        # check if the reactor is not stopped first to prevent calling stop() twice which results
+        # into an error, traceback printed and process hanging until it is killed with SIGKILL.
+        # This sometimes happens when the pytest process is sent a SIGINT signal.
+        # The reactor will stop itself as it register a SIGINT signal for itself and then it is
+        # stopped second time here.
+        # I don't understand when exactly is this happening but I am also observing here that
+        # reactor.running == True and reactor._stopped == True.
+        if not _instances.reactor._stopped:
+            _instances.reactor.stop()
         _instances.gr_twisted.switch()
 
 
