b3138ef404529dccc505f51d770a36497600b14c
Kyle Altendorf <sda@fstab.net>
2021-08-02 11:55:46 -0400

    just trigger twisted to not install a SIGINT handler
    This allows Twisted to still register for SIGCHLD.

--- pytest_twisted.py
+++ pytest_twisted.py
@@ -1,6 +1,7 @@
 import functools
 import inspect
 import itertools
+import signal
 import sys
 import warnings
 
@@ -199,9 +200,9 @@ def init_twisted_greenlet():
         return
 
     if not _instances.reactor.running:
-        _instances.gr_twisted = greenlet.greenlet(
-            lambda: _instances.reactor.run(installSignalHandlers=False),
-        )
+        if signal.getsignal(signal.SIGINT) == signal.default_int_handler:
+            signal.signal(signal.SIGINT, functools.partial(signal.default_int_handler))
+        _instances.gr_twisted = greenlet.greenlet(_instances.reactor.run)
         # give me better tracebacks:
         failure.Failure.cleanFailure = lambda self: None
     else:
