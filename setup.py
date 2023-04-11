2fb1ad8d6761fd6443f8d5e6b358e37b1b58f975
Kyle Altendorf <sda@fstab.net>
2023-04-10 20:59:35 -0400

    qt 6

--- setup.py
+++ setup.py
@@ -17,11 +17,10 @@ setup(
     install_requires=["greenlet", "pytest>=2.3", "decorator"],
     extras_require={
         "dev": ["pre-commit", "black"],
-        "pyside2": [
-            # >= 0.6.3 for PySide2 extra version constraints
-            "qt5reactor[pyside2]>=0.6.3",
-        ],
-        "pyqt5": ["qt5reactor[pyqt5]>=0.6.2"],
+        "pyside2": ["qt5reactor[pyside2] @ git+https://github.com/twisted/qt5reactor@main"],
+        "pyside6": ["qt5reactor[pyside6] @ git+https://github.com/twisted/qt5reactor@main"],
+        "pyqt5": ["qt5reactor[pyqt5] @ git+https://github.com/twisted/qt5reactor@main"],
+        "pyqt6": ["qt5reactor[pyqt6] @ git+https://github.com/twisted/qt5reactor@main"],
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
