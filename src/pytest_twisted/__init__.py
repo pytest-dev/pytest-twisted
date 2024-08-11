cea72c8ab95ff5fc75ff443c611552995e7b6556
Kyle Altendorf <sda@fstab.net>
2024-08-11 16:50:49 -0400

    fixup

--- src/pytest_twisted/__init__.py
+++ src/pytest_twisted/__init__.py
@@ -3,6 +3,8 @@ from pytest_twisted.core import (
     async_yield_fixture,
     blockon,
     ensureDeferred,
+    init_asyncio_reactor,
     init_default_reactor,
+    init_qt5_reactor,
     inlineCallbacks,
 )
